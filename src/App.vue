5a1348d48c2d23276a3bd585749e45d020159153
youssra <youssramortai01@gmail.com>
2023-10-17 12:42:10 +0200

    mijn vue project files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+         <RouterLink to="/stekjes">stekjes</RouterLink>
+        <RouterLink to="/workshops">workshops</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style>
+
+</style>

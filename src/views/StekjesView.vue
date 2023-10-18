288b749d1370638d62fbd158cd8234f31c0c5b4c
youssra <youssramortai01@gmail.com>
2023-10-18 22:07:52 +0200

    barebone basics erin

--- src/views/StekjesView.vue
+++ src/views/StekjesView.vue
@@ -2,5 +2,12 @@
 </script>
 
 <template>
-    <h1>Stekjes</h1>
+    <section>
+        <h2>Alle stekjes</h2>
+        <p>
+          Heb jij een of meerdere plantenstekjes over
+          en zou je die graag willen ruilen voor een 
+          nieuwe plantje? Bekijk hieronder ons aanbod.
+        </p>
+    </section>
 </template>

e6cf6898bf46b6d51304e573692d89908d3d236f
youssra <youssramortai01@gmail.com>
2023-10-26 10:45:14 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,19 @@
-const config = {
-  // api keys
-};
+// const config = {
+//   // api keys
+// };
 
-const db = contentful.initializeApp(config);
-export default db;
+// const db = contentful.initializeApp(config);
+// export default db;
+
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+  space: "fqaoncc3hxft",
+  environment: "master", // defaults to 'master' if not set
+  accessToken: "HndwrgC3YextF_59XSBr3gWhZWi8YsCrMfKdN4CaL24",
+});
+
+client
+  .getEntry("6HbI1gwrOQNrpiSSJirEuK")
+  .then((entry) => console.log(entry))
+  .catch(console.error);
